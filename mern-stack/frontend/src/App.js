26bc651477f9568a338238bcfb5f23322ed694ac
MatiasIstu <matute.mati@hotmail.com>
2020-10-12 17:05:07 -0300

    Test

--- mern-stack/frontend/src/App.js
+++ mern-stack/frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 
 import Navigation from './components/Navigation'
+import NotesList from './components/NotesList'
+import CreateNote from './components/CreateNote'
+import CreateUser from './components/CreateUser'
 
 function App() {
   return (
