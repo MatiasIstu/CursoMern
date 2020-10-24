ce57b40e6f003342c5e1538305f0d6ed8a68f95a
MatiasIstu <matute.mati@hotmail.com>
2020-10-24 14:01:32 -0300

    dd

--- mern-stack/frontend/src/components/CreateNote.js
+++ mern-stack/frontend/src/components/CreateNote.js
@@ -1,10 +1,113 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 export default class CreateNote extends Component {
+
+
+    state = {
+        users: [],
+        userSelected: '',
+        title: '',
+        content: '',
+        date: new Date()
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('http://186.18.40.162:4000/api/users');
+        this.setState({
+            users: res.data.map(user => user.username),
+            userSelected: res.data[0].username
+        })
+
+
+
+    }
+
+    onSubmit = async (e) => {
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+
+        };
+        await axios.post('http://186.18.40.162:4000/api/notes', newNote)
+        e.preventDefault();
+        window.location.href = '/';
+    }
+
+
+    onInputChange = e => {
+        //console.log(e.target.name,e.target.value)
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onChangeDate = date => {
+        this.setState({ date });
+    }
+
     render() {
         return (
-            <div>
-                CREATE NOTE
+            <div className="col-md-6 offset-md-3">
+                <div className="card card-body">
+                    <h4>Create Note</h4>
+                    {/**Select User */}
+                    <div className="form-group">
+                        <select
+                            className="form-control"
+                            name="userSelected"
+                            onChange={this.onInputChange}
+                        >
+                            {
+                                this.state.users.map(user => <option key={user}>
+                                    {user}
+                                </option>)
+                            }
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Title"
+                            name="title"
+                            onChange={this.onInputChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <textarea
+                            name="content"
+                            className="form-control"
+                            placeholder="Content"
+                            onChange={this.onInputChange}
+                            required
+                        >
+                        </textarea>
+                    </div>
+
+                    <div className="form-group">
+                        <DatePicker
+                            className="form-control"
+                            selected={this.state.date}
+                            onChange={this.onChangeDate}
+                        />
+                    </div>
+
+
+
+                    <form onSubmit={this.onSubmit}>
+                        <button type="submit" className="btn btn-primary">
+                            Save
+                        </button>
+                    </form>
+
+                </div>
             </div>
         )
     }
--- mern-stack/frontend/src/components/CreateUser.js
+++ mern-stack/frontend/src/components/CreateUser.js
@@ -3,30 +3,70 @@ import axios from 'axios';
 export default class CreateUser extends Component {
 
     state={
-        users: []
+        users: [],
+        username:''
     }
 
     async   componentDidMount(){
-        const res = await axios.get('http://localhost:4000/api/users')
+        this.getUsers();
+        //console.log(this.state.users)
+    }
+    getUsers=  async () =>{
+        const res = await axios.get('http://186.18.40.162:4000/api/users')
         this.setState({users:res.data});
-        console.log(this.state.users)
+    }
+    onChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value
+        })
     }
 
+    onSubmit = async e =>{
+        e.preventDefault();
+        await axios.post('http://186.18.40.162:4000/api/users',{
+            username : this.state.username
+    })
+        this.setState({username:''})
+        this.getUsers();
+        
+       
+    }
+
+    deleteUser= async (id) =>{
+        await axios.delete('http://186.18.40.162:4000/api/users/'+ id);
+        this.getUsers();
+    }
 
     render() {
         return (
             <div className="row">
                     <div className="col-md-4">
-                       form user
+                       <div className="card card-body">
+                           <h3>Create New User</h3>
+                           <form onSubmit={this.onSubmit} >
+                            <div className="form-group">
+                                <input type="text" className="form-control" value={this.state.username}
+                                onChange={this.onChangeUsername}
+                                />
+                            </div>
+                            <button type ="submit" className="btn btn-primary">
+                                Save
+                            </button>
+                           </form>
+                       </div>
                     </div>
                     <div className="col-md-8">
                          <ul className="list-group">
                         {
                             this.state.users.map(user=> (
-                            <li className="list-group-item list-group-item-action" key = {user._id}>
+                            <li 
+                            className="list-group-item list-group-item-action" 
+                            key = {user._id}
+                            onDoubleClick={()=>this.deleteUser(user._id)}>
                                 {user.username}
                             </li>)
                             )
+                            
                         }
                     </ul>
                     </div>
